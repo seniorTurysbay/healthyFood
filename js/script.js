7cb4e156cb8d2c9e55d6ed04695bf8db7f311b93
seniorTurysbay <d.turysbay@mail.kz>
2021-02-14 20:45:16 +0600

    json-server initialized

--- js/script.js
+++ js/script.js
@@ -244,24 +244,24 @@ window.addEventListener('DOMContentLoaded', function () {
             formData.forEach(function (value, key) {
                 object[key] = value;
             });
-            
+
 
             fetch('server.php', {
                 method: 'POST',
                 headers: {
                     'Content-type': 'application/json'
                 },
-                body:  JSON.stringify(object)
+                body: JSON.stringify(object)
             }).then(data => data.text())
-            .then(data => {
-                console.log(data);
-                showThanksModal(message.success);
-                statusMessage.remove();
-            }).catch(() => {
-                showThanksModal(message.failure);
-            }).finally(()=>{
-                form.reset();
-            })
+                .then(data => {
+                    console.log(data);
+                    showThanksModal(message.success);
+                    statusMessage.remove();
+                }).catch(() => {
+                    showThanksModal(message.failure);
+                }).finally(() => {
+                    form.reset();
+                })
         });
     }
 
@@ -285,4 +285,8 @@ window.addEventListener('DOMContentLoaded', function () {
             closeModal();
         }, 4000)
     }
+
+    fetch('db.json')
+        .then(data => data.json())
+        .then(res => console.log(res));
 });
