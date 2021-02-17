ef894b209b52132fa932127a8b7facb24599d572
Turysbay Dinmukhambet <d.turysbay@yandex.kz>
2021-02-17 21:27:31 +0600

    slide navigation modified version

--- js/script.js
+++ js/script.js
@@ -328,13 +328,39 @@ window.addEventListener("DOMContentLoaded", function() {
 
     const indicators = document.createElement('ol');
     const dots = [];
-    indicators.classList.add('.carousel-indicators')
+    indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `;
     slider.append(indicators);
 
     for (let i = 0; i < slides.length; i++) {
         const dot = document.createElement('li');
         dot.setAttribute('data-slide-to', i + 1);
-        dot.style.classList.add('dot');
+
+        dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `;
         if (i == 0) {
             dot.style.opacity = 1;
         }
